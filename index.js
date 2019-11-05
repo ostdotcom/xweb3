286a8f8fa9beea6011f36b2a47a5ef389349ac9a
shlokgilda <shlok@ost.com>
2019-11-05 13:14:14 +0530

    Updated index for xweb3.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 'use strict';
 
+const Web3 = require('web3');
+
 const rootPrefix = '.',
-  Web3 = require('web3'),
   OstWeb3 = require(rootPrefix + '/lib/web3/OstWeb3'),
-  //OstWSProvider   = require( rootPrefix + '/lib/web3/OstWeb3ProvidersWs' ),
-  Web3PoolFactory = require(rootPrefix + '/lib/web3Pool/ostWeb3PoolFactory'),
-  Web3Pool = require(rootPrefix + '/lib/web3Pool/OstWeb3Pool');
+  Web3Pool = require(rootPrefix + '/lib/web3Pool/OstWeb3Pool'),
+  web3PoolFactory = require(rootPrefix + '/lib/web3Pool/ostWeb3PoolFactory');
 
 module.exports = {
   Web3: Web3,
   OstWeb3: OstWeb3,
-  OstWeb3Pool: {
-    Factory: Web3PoolFactory,
+  ostWeb3Pool: {
+    factory: web3PoolFactory,
     Pool: Web3Pool
   }
 };
